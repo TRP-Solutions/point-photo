be58303117b755a7b607e09ad15bf29fc88889c0
Zatnosk <zatnosk@zatnosk.dk>
2025-05-28 15:39:30 +0200

    Refactored PointPhoto to be fully clientside

--- sample/index.php
+++ sample/index.php
@@ -3,38 +3,58 @@
 PointPhoto is licensed under the Apache License 2.0 license
 https://github.com/TRP-Solutions/boot-some/blob/master/LICENSE
 */
-declare(strict_types=1);
-require_once "../../heal-document/lib/HealDocument.php";
-require_once "../lib/PointPhoto.php";
-
-HealDocument::register_plugin('\TRP\PointPhoto\PointPhoto');
-
-$doc = new HealDocument('html');
-$html = $doc->el('html',['lang'=>'en']);
-$head = $html->el('head');
-$head->el('meta',['charset'=>'utf-8']);
-$head->el('title')->te('PointPhoto sample');
-$head->el('link',['rel'=>'stylesheet','href'=>'../lib/PointPhoto.css']);
-$head->el('script',['src'=>'../lib/PointPhoto.js']);
-$head->el('script',['src'=>'/wild-file/lib/WildFile.js']);
-$body = $html->el('body');
-
-$body->camera();
-
-image($body, 'image1');
-
-image($body, 'image2');
-
-image($body, 'image3');
-
-echo $doc;
-
-function image($body, $name){
-	$image = $body->image($name);
-	$image->img($image);
-	$buttons = $image->el('div',['class'=>'pointphoto-buttons']);
-	$image->camera_button($buttons->el('button')->te('Camera'));
-	$image->upload_button($buttons->el('button')->te('Upload'));
-	$image->clear_button($buttons->el('button')->te('Clear'));
-	$image->warning($image->el('div')->te('Required Image!'));
-}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PointPhoto sample</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../lib/PointPhoto.css">
+	<?php
+	echo '<script src="../lib/PointPhoto.js?'.random_int(1,1000000).'"></script>';
+	?>
+	<style>
+		.container {
+			padding: 1rem;
+			margin-bottom: 1rem;
+			border: 1px solid black;
+			width: max-content;
+		}
+	</style>
+</head>
+<body onload="PointPhoto.init();">
+	<?php
+	if(!empty($_FILES)){
+		foreach($_FILES as $name => $file){
+			if($file['size'] == 0){
+				continue;
+			}
+			echo <<<HTML
+			<p>
+				Input name: $name<br>
+				Uploaded file: $file[name]<br>
+				Type: $file[type]<br>
+				Size: $file[size]
+			</p>
+			HTML;
+		}
+	}
+	?>
+	<div class="pointphoto-camera"></div>
+	<form enctype="multipart/form-data" method="post" action=".">
+		<div class="container">
+			<label>image1</label>
+			<div class="pointphoto-image" data-pointphoto-name="image1" data-pointphoto-preset="preset.svg"></div>
+		</div>
+		<div class="container">
+			<label>image2</label>
+			<div class="pointphoto-image" data-pointphoto-name="image2" data-pointphoto-overlay="overlay.svg"></div>
+		</div>
+		<div class="container">
+			<label>image3</label>
+			<div class="pointphoto-image" data-pointphoto-name="image3"></div>
+		</div>
+		<input type="submit">
+	</form>
+</body>
+</html>
